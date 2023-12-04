267432d14c33734d4f88d8ec632b3744829024cc
kwchu-home <kwchu@DESKTOP-TRCHBSV.localdomain>
2023-12-04 15:32:50 +0100

    include universal header to use for next days

---
+++ include/aoc.h
@@ -0,0 +1,11 @@
+#ifndef AOC_H
+# define AOC_H
+# include "get_next_line.h"
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <ctype.h>
+# include <math.h>
+# include <string.h>
+
+#endif
